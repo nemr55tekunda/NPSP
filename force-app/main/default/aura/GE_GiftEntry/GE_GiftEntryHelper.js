c92ae4b86eb4c342b2853e8de822670fa99ca8e2
Serpent Org-to-Org Deployment <deployment@serpent.local>
2026-06-18 17:03:00 +0000

    chore: sync target org metadata

--- force-app/main/default/aura/GE_GiftEntry/GE_GiftEntryHelper.js
+++
@@ -1,8 +0,0 @@
-({
-    handleCloseModal: function(component) {
-        component.get('v.modal').then(modal => {
-            modal.close();
-            component.set('v.isLoading', false);
-        });
-    }
-})
--- force-app/main/default/aura/REL_RelationshipsApp/REL_RelationshipsApp.app
+++
@@ -1,3 +0,0 @@
-<aura:application description="REL_RelationshipsApp" access="GLOBAL" extends="ltng:outApp">
-    <aura:dependency resource="c:REL_RelationshipsContainer" />
-</aura:application>
