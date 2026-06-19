5ce17534c1a0aaffc3cd9844d517a03f46da0119
Serpent Org-to-Org Deployment <deployment@serpent.local>
2026-06-19 09:24:39 +0000

    chore: sync target org metadata

--- force-app/main/default/aura/ERR_RecordLog/ERR_RecordLogController.js
+++
@@ -1,9 +0,0 @@
-({
-    doInit: function (component, event, helper) {
-        helper.init(component, event, helper);
-    },
-
-    reInit: function (component, event, helper) {
-        helper.refresh(component, event, helper);
-    }
-});
--- force-app/main/default/staticresources/CumulusStaticResources/RP_GettingStarted/js/openNewTab.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    $('div.release-notes > p > a[href]').attr('target', '_blank');
-    $('div.release-notes > div > a[href]').attr('target', '_blank');
-    $('div.release-notes > a[href]').attr('target', '_blank');
-});
